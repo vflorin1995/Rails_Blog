6cb7b5cf497647ad80f7b7d5d44a79cd187783cf
vflorin1995 <vflorin1995@gmail.com>
2023-01-18 20:44:27 +0200

    Fix error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,7 @@ class Comment < ApplicationRecord
 
   def update_comment_counter_for_post
     post = Post.find_by(id: post_id)
-    count = Comment.where(post_id: post.id)
+    count = Comment.where(post_id: post.id).count
     post.update(CommentsCounter: count)
   end
 end
