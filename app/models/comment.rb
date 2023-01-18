d41cd266eafd4ea052ffb0a55f922d2cb08d79b5
vflorin1995 <vflorin1995@gmail.com>
2023-01-18 20:04:27 +0200

    Add methods to model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+
+    def updateCommentCounterForPost
+        post = Post.find_by(id: self.post_id)
+        count = Comment.where(post_id: post.id)
+        post.update(CommentsCounter: count)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :posts
+    has_many :likes
+    has_many :comments
+    
+    def last3posts
+        Post.where(user_id: self.id).order(created_at: :desc).limit(3)
+    end
 end
