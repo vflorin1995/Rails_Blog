f87aae077588c70bb449cf2e3fd1b416ef2e6034
vflorin1995 <vflorin1995@gmail.com>
2023-01-23 19:54:52 +0200

    Add tests for post controller

---
+++ spec/requests/posts_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "PostsControllers", type: :request do
+  describe "GET /index" do
+    it "/users/id/posts test" do
+      get "/users/:id/posts"
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:index)
+      expect(response.body).to include 'This is a list of posts by one user'
+    end
+    it "/users/id/posts/id test" do
+      get "/users/:id/posts/:id"
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:show)
+      expect(response.body).to include 'This is a single post'
+    end
+  end 
+end
