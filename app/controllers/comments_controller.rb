2901515f81acd1bae0cec12a423a6ba5ea624d4d
vflorin1995 <vflorin1995@gmail.com>
2023-01-27 11:44:28 +0200

    Fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
-    def create
-        user = current_user
-        @comment = Comment.new(
-          Text: params[:Text],
-          post_id: params[:post_id],
-          user_id: user.id
-        )
-        return unless @comment.save
-        redirect_to request.original_url
-    end
+  def create
+    user = current_user
+    @comment = Comment.new(
+      Text: params[:Text],
+      post_id: params[:post_id],
+      user_id: user.id
+    )
+    return unless @comment.save
+
+    redirect_to request.original_url
+  end
 end
-  
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,11 @@
 class LikesController < ApplicationController
-    def create
-      user = current_user
-      @like = Like.new(
-        post_id: (params[:post_id]),
-        user_id: user.id
-      )
-  
-      return unless @like.save
-  
-    end
+  def create
+    user = current_user
+    @like = Like.new(
+      post_id: params[:post_id],
+      user_id: user.id
+    )
+
+    return unless @like.save
   end
+end
