cb184e24507b049116bc0415589bf5fbc90e0c55
adelguit <adeelguitoun@gmail.com>
2023-01-31 09:04:29 +0100

    Update test examples

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,15 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'user/index.html.erb', type: :system do
   subject { User.new(Name: 'test', Photo: 'http://google.com', Bio: 'Programmer') }
-  before { subject.save }
+  before(:each) do
+    subject.save
+    visit users_path
+  end
 
   describe 'index page' do
-    it 'shows the right content' do
-      visit users_path
+    it 'shows the Name of the User' do
       expect(page).to have_content('test')
-      expect(page).to have_xpath("//img[@src='http://google.com']")
-      expect(page).to have_content('Number of posts: 0')
+    end
 
+    it 'shows the User Image' do
+      expect(page).to have_xpath("//img[@src='http://google.com']")
+    end
+
+    it 'shows the right Number of Posts' do
+      expect(page).to have_content('Number of posts: 0')
+    end
+
+    it 'Navigate to a specific post' do
       click_link('test')
       sleep(5)
       expect(page).to have_current_path(user_path(subject))
