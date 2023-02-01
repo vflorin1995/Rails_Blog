64f1ba637e5e55d428a70339e5f5addf3836d4bd
vflorin1995 <vflorin1995@gmail.com>
2023-02-01 11:43:14 +0200

    Add user register/ log in

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'post/index.html.erb', type: :system do
-  subject { User.new(Name: 'test', Photo: 'http://google.com', Bio: 'Programmer') }
-  before { subject.save }
+  subject = User.create(Name: 'test', Photo: 'http://google.com', Bio: 'Programmer')
+  Post.create(user_id: subject.id, Title: 'Hello00', Text: 'This is my first post')
+  Post.create(user_id: subject.id, Title: 'Hello10', Text: 'This is my second post')
+  post = Post.create(user_id: subject.id, Title: 'Hello20', Text: 'This is my third post')
+  Post.create(user_id: subject.id, Title: 'Hello30', Text: 'This is my fourth post')
+  Comment.create(Text: 'Hello0', user_id: subject.id, post_id: post.id)
+  Comment.create(Text: 'Hello1', user_id: subject.id, post_id: post.id)
+  Comment.create(Text: 'Hello2', user_id: subject.id, post_id: post.id)
+  Comment.create(Text: 'Hello3', user_id: subject.id, post_id: post.id)
+  Comment.create(Text: 'Hello4', user_id: subject.id, post_id: post.id)
+  Comment.create(Text: 'Hello5', user_id: subject.id, post_id: post.id)
+  sleep(5)
 
   describe 'index page' do
-    it 'shows the right content' do
-      Post.create(user_id: subject.id, Title: 'Hello00', Text: 'This is my first post')
-      Post.create(user_id: subject.id, Title: 'Hello10', Text: 'This is my second post')
-      post = Post.create(user_id: subject.id, Title: 'Hello20', Text: 'This is my third post')
-      Post.create(user_id: subject.id, Title: 'Hello30', Text: 'This is my fourth post')
-      Comment.create(Text: 'Hello0', user_id: subject.id, post_id: post.id)
-      Comment.create(Text: 'Hello1', user_id: subject.id, post_id: post.id)
-      Comment.create(Text: 'Hello2', user_id: subject.id, post_id: post.id)
-      Comment.create(Text: 'Hello3', user_id: subject.id, post_id: post.id)
-      Comment.create(Text: 'Hello4', user_id: subject.id, post_id: post.id)
-      Comment.create(Text: 'Hello5', user_id: subject.id, post_id: post.id)
-      sleep(5)
-
+    it 'Can see user profile picture' do
       visit user_posts_path(subject)
       expect(page).to have_xpath("//img[@src = 'http://google.com']")
+    end
+
+    it '' do
       expect(page).to have_content('test')
       expect(page).to have_content('Number of posts: 4')
       expect(page).to have_content('Hello20')
