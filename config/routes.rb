d8e9c891ed1c8916d0264fa95b7f0d4e7e96913c
adelguit <adeelguitoun@gmail.com>
2023-02-01 10:45:32 +0100

    Add the routes, view and controller for reseting the password

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    # passwords: 'users/passwords'
+    passwords: 'users/passwords'
   }
   root "users#index"
 
